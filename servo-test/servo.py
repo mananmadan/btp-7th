7c81a0f36ad665ef3aa122de8c80408e22fa28fc
mananmadan <madan.manan1@gmail.com>
2021-09-06 11:51:44 +0530

    error resolution

--- servo-test/servo.py
+++ servo-test/servo.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 import time
 
 class Servo:
-        pin = 0
-        def __init__(self,pinnumber):
-            self.pin = pinnumber
-            # Set GPIO numbering mode
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(self.pin,GPIO.OUT)
-            self.pwm = GPIO.PWM(self.pin,50) # Note 11 is pin, 50 = 50Hz pulse
+ pin = 0
+ def __init__(self,pinnumber):
+    self.pin = pinnumber
+    # Set GPIO numbering mode
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(self.pin,GPIO.OUT)
+    self.pwm = GPIO.PWM(self.pin,50) # Note 11 is pin, 50 = 50Hz pulse
 
-        def SetAngle(self,angle):
-	        duty = angle / 18 + 2
-            print("Duty",duty)
-	        GPIO.output(self.pin, True)
-	        self.pwm.ChangeDutyCycle(duty)
-	        time.sleep(1)
-	        GPIO.output(self.pin, False)
+ def SetAngle(self,angle):
+    duty = angle / 18 + 2
+    print("Duty",duty)
+	GPIO.output(self.pin, True)
+	self.pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+	GPIO.output(self.pin, False)
