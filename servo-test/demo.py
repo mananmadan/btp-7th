b427feca56b4894cdec55d4fd889969a243877ba
mananmadan <madan.manan1@gmail.com>
2021-09-06 10:23:31 +0530

    servo class containing set angle function

---
+++ servo-test/demo.py
@@ -0,0 +1,7 @@
+from servo import Servo
+
+servo1 = Servo(11)
+servo1.SetAngle(45)
+
+servo2 = Servo(12)
+servo2.SetAngle(45)
---
+++ servo-test/servo.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+class Servo:
+        pin = 0
+        def __init__(self,pinnumber) -> None:
+            self.pin = pinnumber
+            # Set GPIO numbering mode
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.pin,GPIO.OUT)
+            self.pwm = GPIO.PWM(self.pin,50) # Note 11 is pin, 50 = 50Hz pulse
+
+
+            
+        def SetAngle(self,angle):
+	        duty = angle / 18 + 2
+	        GPIO.output(self.pin, True)
+	        self.pwm.ChangeDutyCycle(duty)
+	        time.sleep(1)
+	        GPIO.output(self.pin, False)
+            self.pwm.ChangeDutyCycle(0)
