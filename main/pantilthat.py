59a1994367e1e85d5f83bc419764b682d5c3ec5f
gupta2022 <guptagaurav2022@gmail.com>
2021-11-24 13:42:30 +0530

    minor change

--- main/pantilthat.py
+++ main/pantilthat.py
@@ -11,11 +11,23 @@
  - if error is neg.. make servopwm less by 0.5
  - if error is pos .. make servopwm more by 0.5
 '''
+import RPi.GPIO as GPIO
+import time
+
+
 def servo_enable(pin,val):
     ## code for servo enable false or true
+    # Set GPIO numbering mode
+    GPIO.setmode(GPIO.BOARD)
+
+    # Set pin 11 as an output, and set servo1 as pin 11 as PWM
+    GPIO.setup(11,GPIO.OUT)
+    GPIO.setup(12,GPIO.OUT)
     print("servo enable",pin,val)
 
-def publish(pin,val): 
+def publish(pin,val):
+    servo=GPIO.PWM(pin,50)
+    servo.start(0)
     ## code for publishing max value
     print("Publish",pin,val)
 
@@ -30,7 +42,7 @@ def pan(arg1,cp):
             cp = cp - 0.5
         print("Pos")
     print("cp:",cp)
-    publish(1,cp) ## publish val on pan pin
+    publish(11,cp) ## publish val on pan pin
     return cp
 
 def tilt(arg1,ct):
@@ -40,9 +52,9 @@ def tilt(arg1,ct):
         if ct < 12.0:
             ct = ct + 0.5
     if arg1 > 0:
-        if cp > 0.0:
+        if ct > 0.0:
             ct = ct - 0.5
         print("Pos")
     print("ct:",ct)
-    publish(2,ct) ## publish val on tilt pin
+    publish(12,ct) ## publish val on tilt pin
     return ct
